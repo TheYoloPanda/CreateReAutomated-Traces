3e7eebd3686dfc4b782dbf22206e937e481ef03d
TYPz <marchetti.alessandro2d@gmail.com>
2026-05-07 21:30:08 +0200

    First Commit

---
+++ src/main/java/com/typ/traces/CreateReAutomatedTracesClient.java
@@ -0,0 +1,31 @@
+package com.typ.traces;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = CreateReAutomatedTraces.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = CreateReAutomatedTraces.MODID, value = Dist.CLIENT)
+public class CreateReAutomatedTracesClient {
+    public CreateReAutomatedTracesClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        CreateReAutomatedTraces.LOGGER.info("HELLO FROM CLIENT SETUP");
+        CreateReAutomatedTraces.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
